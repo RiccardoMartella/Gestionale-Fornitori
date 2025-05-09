7b8ba21f946a340712a399ca46bb6b67ebe9a402
Riccardo Martella <riccardomartella77@gmail.com>
2025-05-09 23:08:06 +0200

    fix: rail

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -18,7 +18,6 @@
                             Nuovo Fornitore
                         </a>
                     </div>
-                    
                     @if(session('success'))
                         <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-6" role="alert">
                             <p>{{ session('success') }}</p>
